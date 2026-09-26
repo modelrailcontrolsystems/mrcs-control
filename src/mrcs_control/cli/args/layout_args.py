97a7ccee9d935437cb70805160f33ca312927a6e
Bruno Beloff <bruno.beloff@southcoastscience.com>
2026-09-26 15:31:16 +0100

    Added platform path navigation

--- src/mrcs_control/cli/args/layout_args.py
+++ src/mrcs_control/cli/args/layout_args.py
@@ -11,12 +11,15 @@ from argparse import Action
 from mrcs_core.cli.args.multimode_args import MultimodeArgs
 from mrcs_core.equipment.block.block_enums import BlockHeading
 from mrcs_core.inventory.layout.location import Location
+from mrcs_core.inventory.platform.platform_label import PlatformLabel
 
 
 # --------------------------------------------------------------------------------------------------------------------
 
-class PathAction(Action):
-    """argparse action for layout path command (HEADING B1 S1 B2 S2)"""
+class SegmentPathAction(Action):
+    """
+    argparse action for layout segment-path command (HEADING Station1/1 Station2/2)
+    """
 
 
     def __call__(self, parser, namespace, values, option_string=None):
@@ -42,6 +45,35 @@ class PathAction(Action):
         setattr(namespace, self.dest, (heading, values[1], values[2]))
 
 
+class PlatformPathAction(Action):
+    """
+    argparse action for layout platform-path command (HEADING B1/S1 B2/S2)
+    """
+
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        if values is None:
+            return
+
+        opt = option_string if option_string else '-p/--path'
+
+        heading = values[0].upper()
+        if heading not in ('UP', 'DN'):
+            parser.error(f"argument {opt}: HEADING must be 'UP' or 'DN' (got '{values[0]}')")
+
+        try:
+            PlatformLabel.construct_from_shortform(values[1])
+        except ValueError:
+            parser.error(f"argument {opt}: START is malformed' (got '{values[1]}')")
+
+        try:
+            PlatformLabel.construct_from_shortform(values[2])
+        except ValueError:
+            parser.error(f"argument {opt}: END is malformed' (got '{values[2]}')")
+
+        setattr(namespace, self.dest, (heading, values[1], values[2]))
+
+
 # --------------------------------------------------------------------------------------------------------------------
 
 class LayoutArgs(MultimodeArgs):
@@ -55,8 +87,13 @@ class LayoutArgs(MultimodeArgs):
 
         group = self._parser.add_mutually_exclusive_group(required=False)
 
-        group.add_argument('-p', '--path', action=PathAction, nargs=3, metavar=('HEADING', 'START', 'END'),
+        group.add_argument('-s', '--segment-path', action=SegmentPathAction, nargs=3,
+                           metavar=('HEADING', 'START', 'END'),
                            help='find path from B1/S1 to B2/S2 for HEADING: { UP | DN }')
+
+        group.add_argument('-p', '--platform-path', action=PlatformPathAction, nargs=3,
+                           metavar=('HEADING', 'START', 'END'),
+                           help='find path from Station/1 to Station/2 for HEADING: { UP | DN }')
 
         self._args = self._parser.parse_args()
 
@@ -69,29 +106,56 @@ class LayoutArgs(MultimodeArgs):
 
 
     @property
-    def path(self):
-        return self._args.path
-
-
-    @property
     def path_heading(self):
-        if self._args.path is None:
+        if self._args.segment_path:
+            return BlockHeading.UP if self._args.segment_path[0] == 'UP' else BlockHeading.DOWN
+
+        if self._args.platform_path:
+            return BlockHeading.UP if self._args.platform_path[0] == 'UP' else BlockHeading.DOWN
+
+        return None
+
+
+    @property
+    def segment_path(self):
+        return self._args.segment_path
+
+
+    @property
+    def segment_path_start(self):
+        if self._args.segment_path is None:
             return None
-        return BlockHeading.UP if self._args.path[0] == 'UP' else BlockHeading.DOWN
+        return Location.construct_from_shortform(self._args.segment_path[1])
 
 
     @property
-    def path_start(self):
-        return None if self._args.path is None else Location.construct_from_shortform(self._args.path[1])
+    def segment_path_end(self):
+        if self._args.segment_path is None:
+            return None
+        return Location.construct_from_shortform(self._args.segment_path[2])
 
 
     @property
-    def path_end(self):
-        return None if self._args.path is None else Location.construct_from_shortform(self._args.path[2])
+    def platform_path(self):
+        return self._args.platform_path
+
+
+    @property
+    def platform_path_start(self):
+        if self._args.platform_path is None:
+            return None
+        return PlatformLabel.construct_from_shortform(self._args.platform_path[1])
+
+
+    @property
+    def platform_path_end(self):
+        if self._args.platform_path is None:
+            return None
+        return PlatformLabel.construct_from_shortform(self._args.platform_path[2])
 
 
     # ----------------------------------------------------------------------------------------------------------------
 
     def __str__(self, *args, **kwargs):
-        return (f'LayoutArgs:{{layout:{self.layout}, path:{self.path}, '
-                f'indent:{self.indent}, verbose:{self.verbose}}}')
+        return (f'LayoutArgs:{{layout:{self.layout}, segment_path:{self.segment_path}, '
+                f'platform_path:{self.platform_path}, indent:{self.indent}, verbose:{self.verbose}}}')
